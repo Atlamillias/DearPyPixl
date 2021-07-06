4c169cf4c61b3c809af34645ef33ae77f42a77b6
Anthony Doupe <atlamillias@gmail.com>
2021-07-06 13:28:11 -0700

    Fixed circular import (base classes).

--- dpgwidgets/__init__.py
+++ dpgwidgets/__init__.py
@@ -3,4 +3,23 @@ import sys
 sys.path.append("./dpgwrap/")
 
 from dearpygui import core as idpg, dearpygui as dpg
+
+# order is to avoid circular imports
+from dpgwrap._item import Item, Context
+from dpgwidgets.widget import Container, Widget
+
+from dpgwidgets.app import Application, Viewport
 from dpgwrap import *
+
+
+def update_wrappers():
+    import importlib
+    import dpgwrap
+    from dpgwidgets import _generate
+
+    _generate.main()
+
+    importlib.reload(dpgwrap)
+
+    [importlib.import_module(module, dpgwrap)
+     for module in dpgwrap.__all__]
