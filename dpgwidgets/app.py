342da7d5e57b646d1ab8258df1b19524b631eb31
Anthony Doupe <atlamillias@gmail.com>
2021-07-13 16:58:23 -0700

    Git cache

---
+++ dpgwidgets/app.py
@@ -0,0 +1,352 @@
+from typing import Any, Callable
+from contextlib import contextmanager
+
+from dpgwidgets import dpg
+from dpgwidgets.constants import Registry as _Reg
+
+
+# Application and Viewport classes kinda share
+# responsibilities. This is mostly because at the moment
+# DPG only supports 1 viewport. However in the future
+# it may support several, so they will need to be 
+# more de-coupled.
+class Application:
+    called_on_start = []
+    called_on_exit = []
+
+    def __init__(
+        self, 
+        enable_staging: bool = False, 
+        enable_docking: bool = False,
+        dock_space: bool = False,
+        show_dev_tools:bool = False
+    ):
+        self.viewport = Viewport()
+
+        # Registries (WIP)
+        self.__theme = None
+        self.__docking_enabled = False
+        self.__dev_tools = True if show_dev_tools else False
+        self.__staging_mode = enable_staging
+        
+        if enable_docking:
+            dpg.enable_docking(dock_space=dock_space)
+            self.__docking_enabled = True
+
+        self.viewport.setup()
+        self.viewport.show()
+
+
+    def register_callbacks(self):
+        dpg.set_start_callback(lambda: [f() for f in self.called_on_start])
+        dpg.set_exit_callback(lambda: [f() for f in self.called_on_exit])
+
+    def start(self):
+        self.register_callbacks()
+        if self.__dev_tools:
+            self.viewport._show_dev_toolbar()
+
+        while dpg.is_dearpygui_running():
+            dpg.render_dearpygui_frame()
+
+        dpg.cleanup_dearpygui()
+
+    @staticmethod
+    def stop():
+        dpg.stop_dearpygui()
+
+
+    ## Decorators ##
+    def on_start(self, func):
+        """Adds <func> to a list of functions that will be
+        called before the main loop (in the order that they are added)."""
+        self.called_on_start.append(func)
+
+        return func
+
+    def on_exit(self, func):
+        """Adds <func> to a list of functions that will be
+        called as the main loop ends (in the order that they are added)."""
+        self.called_on_exit.append(func)
+
+        return func
+
+    ## Registries ##
+    @property
+    def fonts(self):
+        return self.__fonts
+
+
+    @property
+    def values(self):
+        return self.__values
+
+    @property
+    def textures(self):
+        return self.__textures
+
+
+    ## Handler ##
+    @property
+    def handle(self):
+        return self.__handler
+
+    def unhandle(self, handler_type: str, callback: Callable):
+        return self.__handler.unhandle(handler_type, callback)
+    
+
+    ## Theme ##
+    @property
+    def theme(self):
+        return self.__theme
+
+    @property
+    def color(self):
+        return self.__theme.color
+
+    @property
+    def style(self):
+        return self.__theme.style
+
+
+    ## Misc. ##
+    @property
+    def time_elapsed(self):
+        return dpg.get_total_time()
+    
+    @property
+    def global_font_scale(self):
+        return dpg.get_global_font_scale()
+
+    @global_font_scale.setter
+    def global_font_scale(self, value: float):
+        dpg.set_global_font_scale(value)
+
+    @property
+    def staging_mode(self):
+        return self.__staging_mode
+
+    @staging_mode.setter
+    def staging_mode(self, value: bool):
+        self.__staging_mode = value
+        dpg.set_staging_mode(mode=value)
+
+    @property
+    def docking_enabled(self):
+        return self.__docking_enabled
+
+    with dpg.font_registry(id=_Reg.FONT.value):
+        pass
+    with dpg.handler_registry(id=_Reg.APPHANDLER.value):
+        pass
+    with dpg.texture_registry(id=_Reg.TEXTURE.value):
+        pass
+    with dpg.value_registry(id=_Reg.VALUE.value):
+        pass
+
+
+class Viewport:
+    # Note: DPG currently only allows 1 viewport
+    # so only call once
+    __config = {}
+    __primary_window = None
+    called_on_resize = []
+
+    def __init__(
+        self,
+        title: str = "Application",
+        small_icon: str = '',
+        large_icon: str = '',
+        width: int = 1280,
+        height: int = 800,
+        x_pos: int = 100,
+        y_pos: int = 100,
+        min_width: int = 250,
+        max_width: int = 10000,
+        min_height: int = 250,
+        max_height: int = 10000,
+        resizable: bool = True,  # not working?
+        vsync: bool = True,
+        always_on_top: bool = False,
+        maximized_box: bool = True,
+        minimized_box: bool = True,
+        border: bool = True,
+        caption: bool = True,
+        overlapped: bool = True,
+        clear_color: list[float] = [0, 0, 0, 255],
+    ):
+        self.title = title
+        self.small_icon = small_icon
+        self.large_icon = large_icon
+        self.width = width
+        self.height = height
+        self.x_pos = x_pos
+        self.y_pos = y_pos
+        self.min_width = min_width
+        self.max_width = max_width
+        self.min_height = min_height
+        self.max_height = max_height
+        self.resizable = resizable
+        self.vsync = vsync
+        self.always_on_top = always_on_top
+        self.maximized_box = maximized_box
+        self.minimized_box = minimized_box
+        self.border = border
+        self.caption = caption
+        self.overlapped = overlapped
+        self.clear_color = clear_color
+
+        self.__config = {
+            "title": title,
+            "small_icon": small_icon,
+            "large_icon": large_icon,
+            "width": width,
+            "height": height,
+            "x_pos": x_pos,
+            "y_pos": y_pos,
+            "min_width": min_width,
+            "max_width": max_width,
+            "min_height": min_height,
+            "max_height": max_height,
+            "resizable": resizable,
+            "vsync": vsync,
+            "always_on_top": always_on_top,
+            "maximized_box": maximized_box,
+            "minimized_box": minimized_box,
+            "border": border,
+            "caption": caption,
+            "overlapped": overlapped,
+            "clear_color": clear_color,
+        }
+
+        # __getitem__ will ask dpg for all of the above
+        # if this is called before setting instance attrs
+        self.__id = dpg.create_viewport(**self.__config)
+
+    def __str__(self):
+        return f"{self.__id}"
+
+    def __repr__(self):
+        return f"{self.__class__.__qualname__}"
+
+    def __getattr__(self, attr):
+        if attr in self.__config:
+            try:
+                return dpg.get_viewport_configuration(self.__id)
+            except KeyError:
+                return super().__getattr__(attr)
+
+        return super().__getattr__(attr)
+
+    def __setattr__(self, attr, value):
+        if attr in self.__config:
+            dpg.configure_viewport(self.__id, **{attr: value})
+        else:
+            super().__setattr__(attr, value)
+
+    def _show_dev_toolbar(self):
+        from dpgwrap.containers import Menu, ViewportMenuBar
+        from dpgwrap.widgets import MenuItem
+
+        with ViewportMenuBar():
+            with Menu("Development") as dev:
+                MenuItem("Documentation", callback=show_documentation)
+                MenuItem("Debug", callback=show_debug)
+                MenuItem("Style Editor", callback=show_style_editor)
+                MenuItem("Metrics", callback=show_metrics)
+                MenuItem("About", callback=show_about)
+                MenuItem("Font Manager", callback=show_font_manager)
+                MenuItem("Item Registry", callback=show_item_registry)
+                MenuItem("Logger", callback=show_logger)
+
+    @property
+    def id(self):
+        return self.__id
+
+    def configure(self, **config):
+        """Updates the viewport configuration."""
+        dpg.configure_viewport(self.__id, **config)
+
+    def configuration(self, option=None):
+        """Returns the entire current viewport configuration, or
+        <option> if specified."""
+        if option:
+            return dpg.get_viewport_configuration(self.__id)[option]
+
+        return dpg.get_viewport_configuration(self.__id)
+
+    def maximize(self):
+        """Maximize the viewport"""
+        dpg.maximize_viewport(self.__id)
+
+    def minimize(self):
+        """Minimize the viewport."""
+        dpg.minimize_viewport(self.__id)
+
+    def setup(self):
+        dpg.setup_dearpygui(viewport=self.__id)
+
+    def show(self):
+        """Display the viewport."""
+        dpg.show_viewport(self.__id)
+
+    # Decorator
+    def on_resize(self, func):
+        """Updates the viewport callback collection."""
+        self.called_on_resize.add(func)
+        dpg.set_viewport_resize_callback(lambda: [
+            f() for f in self.called_on_resize])
+
+        return func
+
+    @property
+    def primary_window(self):
+        return self.__primary_window
+
+    def set_primary_window(self, state: bool, window: int = None):
+        if window is not None:
+            self.__primary_window = window
+
+        dpg.set_primary_window(int(self.__primary_window), state)
+
+
+## Misc. (most are copy/paste from dearpygui) ##
+@contextmanager
+def mutex():
+   try:
+       yield dpg.lock_mutex()
+   finally:
+       dpg.unlock_mutex()
+
+
+def show_style_editor(sender: str = "", data: Any = None):
+    dpg.show_tool(dpg.mvTool_Style)
+
+
+def show_metrics(sender: str = "", data: Any = None):
+    dpg.show_tool(dpg.mvTool_Metrics)
+
+
+def show_about(sender: str = "", data: Any = None):
+    dpg.show_tool(dpg.mvTool_About)
+
+
+def show_debug(sender: str = "", data: Any = None):
+    dpg.show_tool(dpg.mvTool_Debug)
+
+
+def show_documentation(sender: str = "", data: Any = None):
+    dpg.show_tool(dpg.mvTool_Doc)
+
+
+def show_font_manager(sender: str = "", data: Any = None):
+    dpg.show_tool(dpg.mvTool_Font)
+
+
+def show_item_registry(sender: str = "", data: Any = None):
+    dpg.show_tool(dpg.mvTool_ItemRegistry)
+
+
+def show_logger(sender: str = "", data: Any = None):
+    from dearpygui import logger
+    logger.mvLogger()
---
+++ dpgwidgets/handler.py
@@ -0,0 +1,205 @@
+from __future__ import annotations
+from typing import TYPE_CHECKING, Callable, Any
+import functools
+import inspect
+
+from dpgwrap._item import Item, ContextSupport
+
+from dpgwidgets import dpg
+
+
+if TYPE_CHECKING:
+    from .widget import Widget
+
+
+# Handlers are injected into HandlerRegistry subclasses at runtime.
+# This could have been done using metaclasses and descriptors (probably will)
+# but I wanted the code itself to be readable.
+
+_HANDLER_METHOD = """
+def {mname}(self, callback: Callable = None, *, user_data: Any = None, **kwargs):
+    @functools.wraps(callback)
+    def wrapper(callback):
+        handler = dpg.{cmd}(
+            self.parent.id,
+            callback=callback,
+            user_data=user_data,
+            **kwargs
+        )
+        self.handlers.setdefault(self.{mname}.__name__, []).append(
+            (callback, handler),
+        )
+        return callback
+
+    if not callback:
+        return wrapper
+
+    handler = dpg.{cmd}(
+        self.parent.id,
+        callback=callback,
+        user_data=user_data,
+        **kwargs
+        )
+    self.handlers.setdefault(self.{mname}.__name__, []).append(
+        (callback, handler),
+    )
+    return callback
+"""
+
+class HandlerRegistry:  # Mixin
+    __handlers: dict[list[tuple]] = {}
+
+    @property
+    def handlers(self):
+        return self.__handlers
+
+    def unhandle(self, handler_type: str, callback: Callable):
+        handler = self._pop_handler(handler_type, callback)
+
+        dpg.delete_item(handler)
+
+    def _pop_handler(self, handler_type: str, callback: Callable):
+        key = self.__handlers[handler_type]
+        for idx, h_info in enumerate(key):
+            if h_info[0] == callback:
+                handler = key.pop(idx)[-1]
+                return handler
+
+        return None
+
+
+
+# Method "templates" are defined in HandlerRegistry subclasses
+# to allow intellisense and language servers to do its thing. They
+# are actually redefined at runtime using the
+# __handler_method" code template.
+class AppHandler(Item, ContextSupport, HandlerRegistry):
+    _command = dpg.add_handler_registry
+
+    _HANDLERS = {
+        "while_key_down": "add_key_down_handler",  # mvKeyDownHandler
+        "on_key_press": "add_key_press_handler",  # mvKeyPressHandler
+        "on_key_up": "add_key_release_handler",  # mvKeyReleaseHandler
+        "while_mouse_key_down":  "add_mouse_down_handler",  # mvMouseDownHandler
+        "on_mouse_key_click": "add_mouse_click_handler",  # mvMouseClickHandler
+        # mvMouseDoubleClickHandler
+        "on_mouse_key_double_click": "add_mouse_double_click_handler",
+        "on_mouse_key_up": "add_mouse_release_handler",  # mvMouseReleaseHandler
+        "on_mouse_wheel_scroll": "add_mouse_wheel_handler",  # mvMouseWheelHandler
+        "on_mouse_move": "add_mouse_move_handler",  # mvMouseMoveHandler
+        "on_mouse_drag": "add_mouse_drag_handler",  # mvMouseDragHandler
+    }
+
+    def __init__(self, show: bool = True, label: str = None, **kwargs):
+        super().__init__(show=show, label=label, **kwargs)
+        self.__handlers = {}
+        self.show = show
+        self.label = label
+
+    def while_key_down(self, callback: Callable = None, *, user_data: Any = None, **kwargs):
+        ...
+
+    def on_key_press(self, callback: Callable = None, *, user_data: Any = None, **kwargs):
+        ...
+
+    def on_key_up(self, callback: Callable = None, *, user_data: Any = None, **kwargs):
+        ...
+
+    def while_mouse_key_down(self, callback: Callable = None, *, user_data: Any = None, **kwargs):
+        ...
+
+    def on_mouse_key_click(self, callback: Callable = None, *, user_data: Any = None, **kwargs):
+        ...
+
+    def on_mouse_key_double_click(self, callback: Callable = None, *, user_data: Any = None, **kwargs):
+        ...
+
+    def on_mouse_key_up(self, callback: Callable = None, *, user_data: Any = None, **kwargs):
+        ...
+
+    def on_mouse_wheel_scroll(self, callback: Callable = None, *, user_data: Any = None, **kwargs):
+        ...
+
+    def on_mouse_move(self, callback: Callable = None, *, user_data: Any = None, **kwargs):
+        ...
+
+    def on_mouse_drag(self, callback: Callable = None, *, user_data: Any = None, **kwargs):
+        ...
+
+
+# DPG doesn't have a specific widget handler registry item.
+# Probably because there is a parenting widget instead
+class WidgetHandler(HandlerRegistry):
+    _HANDLERS = {
+        "while_active": "add_active_handler",  # mvActivatedHandler
+        "on_activation": "add_activated_handler",  # mvActiveHandler
+        "on_click": "add_clicked_handler",  # mvClickedHandler
+        # mvDeactivatedAfterEditHandler
+        "on_deactivation_after_edit": "add_deactivated_after_edit_handler",
+        "on_deactivation": "add_deactivated_handler",  # mvDeactivatedHandler
+        "on_edit": "add_edited_handler",  # mvEditedHandler
+        "on_focus": "add_focus_handler",  # mvFocusHandler
+        "on_hover": "add_hover_handler",  # mvHoverHandler
+        "on_resize": "add_resize_handler",  # mvResizeHandler
+        "on_toggle_open": "add_toggled_open_handler",  # mvToggledOpenHandler
+        "on_visible": "add_visible_handler",  # mvVisibleHandler
+    }
+
+    def __init__(self, parent: "Widget"):
+        self.__handlers = {}
+        self.parent = parent
+
+    def while_active(self, callback: Callable = None, *, user_data: Any = None, **kwargs):
+        ...
+
+    def on_activation(self, callback: Callable = None, *, user_data: Any = None, **kwargs):
+        ...
+    
+    def on_deactivation(self, callback: Callable = None, *, user_data: Any = None, **kwargs):
+        ...
+
+    def on_deactivation_after_edit(self, callback: Callable = None, *, user_data: Any = None, **kwargs):
+        ...
+
+    def on_click(self, callback: Callable = None, *, user_data: Any = None, button=-1, **kwargs):
+        ...
+
+    def on_edit(self, callback: Callable = None, *, user_data: Any = None, **kwargs):
+        ...
+
+    def on_focus(self, callback: Callable = None, *, user_data: Any = None, **kwargs):
+        ...
+
+    def on_hover(self, callback: Callable = None, *, user_data: Any = None, **kwargs):
+        ...
+    
+    def on_resize(self, callback: Callable = None, *, user_data: Any = None, **kwargs):
+        ...
+
+    def on_toggle_open(self, callback: Callable = None, *, user_data: Any = None, **kwargs):
+        ...
+
+    def on_visible(self, callback: Callable = None, *, user_data: Any = None, **kwargs):
+        ...
+
+
+def _patch_handlers():
+    filepath = ""
+    template_str = _HANDLER_METHOD
+
+    for m_name, cmd in AppHandler._HANDLERS.items():
+        m_template = template_str.format(cmd=cmd, mname=m_name)
+        method = compile(m_template, filename=filepath, mode="exec")
+        exec(method)  # m_name
+
+        setattr(AppHandler, m_name, eval(m_name))
+
+    for m_name, cmd in WidgetHandler._HANDLERS.items():
+        m_template = template_str.format(cmd=cmd, mname=m_name)
+        method = compile(m_template, filename=filepath, mode="exec")
+        exec(method)  # m_name
+
+        setattr(WidgetHandler, m_name, eval(m_name))
+
+
+_patch_handlers()
