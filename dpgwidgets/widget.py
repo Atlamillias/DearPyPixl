70db49db5fee37cce74dd37e1d60f9e9f49f7edc
Anthony Doupe <atlamillias@gmail.com>
2021-07-14 15:18:01 -0700

    Added new handler support

--- dpgwidgets/widget.py
+++ dpgwidgets/widget.py
@@ -3,30 +3,17 @@ from typing import Callable
 
 from dpgwrap._item import Item, ContextSupport
 from dpgwidgets import dpg
-from dpgwidgets.handler import WidgetHandler
+from dpgwidgets.handler import HandlerSupport
 from dpgwidgets.theme import ThemeSupport, Theme
 
 
-class Widget(Item, ThemeSupport, metaclass=ABCMeta):
+class Widget(Item, ThemeSupport, HandlerSupport, metaclass=ABCMeta):
     @abstractmethod
     def _command() -> Callable: ...
 
-    def __init__(self, theme: Theme = None, **kwargs):
+    def __init__(self, **kwargs):
         super().__init__(**kwargs)
-        self.__handler = WidgetHandler(self)
-        self.theme = theme or Theme(f"{self.label}_{self.id}")
-        
-    # WidgetHandler
-    @property
-    def handle(self):
-        return self.__handler
 
-    def unhandle(self, handler_type: str, callback: Callable):
-        return self.__handler.unhandle(handler_type, callback)
-
-
-
-    # Misc
     def move_up(self):
         dpg.move_item_up(self.id)
 
