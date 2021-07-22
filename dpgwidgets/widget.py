fd8b5b42b5802fea9597e34aa6ba95fda99384e7
Anthony Doupe <atlamillias@gmail.com>
2021-07-22 13:47:11 -0700

    Added properties for states.

--- dpgwidgets/widget.py
+++ dpgwidgets/widget.py
@@ -1,7 +1,7 @@
 from abc import ABCMeta, abstractmethod
 from typing import Callable
 
-from dpgwidgets import dpg, Item, ContextSupport
+from dpgwidgets import idpg, Item, ContextSupport
 from dpgwidgets.handler import HandlerSupport
 from dpgwidgets.theme import ThemeSupport
 
@@ -14,18 +14,69 @@ class Widget(Item, ThemeSupport, HandlerSupport, metaclass=ABCMeta):
         super().__init__(**kwargs)
 
     def move_up(self) -> None:
-        dpg.move_item_up(self.id)
+        idpg.move_item_up(self.id)
 
     def move_down(self) -> None:
-        dpg.move_item_down(self.id)
+        idpg.move_item_down(self.id)
 
     def move(self, parent: int, before: int) -> None:
         """Move a widget to another <parent> before <before>."""
-        dpg.move_item(self.id, parent, before)
+        idpg.move_item(self.id, parent, before)
 
     def slots(self, slot: int = -1) -> dict:
         """Returns the item's slots and the list of children (id) in each
         as a dict ({slot: [],...})."""
+
+
+    ## State-getters ##
+    @property
+    def is_container(self):
+        return idpg.get_item_info(self.__id)["container"]
+
+    @property
+    def is_hovered(self):
+        return idpg.get_item_state(self.__id)["hovered"]
+
+    @property
+    def is_active(self):
+        return idpg.get_item_state(self.__id)["active"]
+
+    @property
+    def is_focused(self):
+        return idpg.get_item_state(self.__id)["focused"]
+
+    @property
+    def is_clicked(self):
+        return idpg.get_item_state(self.__id)["clicked"]
+
+    @property
+    def is_visible(self):
+        return idpg.get_item_state(self.__id)["visible"]
+
+    @property
+    def is_edited(self):
+        return idpg.get_item_state(self.__id)["edited"]
+
+    @property
+    def is_activated(self):
+        return idpg.get_item_state(self.__id)["activated"]
+
+    @property
+    def is_deactivated(self):
+        return idpg.get_item_state(self.__id)["deactivated"]
+
+    @property
+    def is_deactivated_after_edit(self):
+        return idpg.get_item_state(self.__id)["deactivated_after_edit"]
+
+    @property
+    def is_toggled_open(self):
+        return idpg.get_item_state(self.__id)["toggled_open"]
+
+    @property
+    def is_ok(self):
+        return idpg.get_item_state(self.__id)["ok"]
+
 
 
 class Container(Widget, ContextSupport, metaclass=ABCMeta):
@@ -36,40 +87,40 @@ class Container(Widget, ContextSupport, metaclass=ABCMeta):
     # x_scroll
     @property
     def y_scroll_pos(self):
-        return dpg.get_y_scroll(self.id)
+        return idpg.get_y_scroll(self.id)
     @y_scroll_pos.setter
     def y_scroll_pos(self, value):  # -1.0 will set it to max/end
-        dpg.set_y_scroll(self.id, value)
+        idpg.set_y_scroll(self.id, value)
 
     @property
     def y_scroll_max(self):
-        return dpg.get_y_scroll_max(self.id)
+        return idpg.get_y_scroll_max(self.id)
 
 
     # y_scroll
     @property
     def x_scroll_pos(self):
-        return dpg.get_x_scroll(self.id)
+        return idpg.get_x_scroll(self.id)
     @x_scroll_pos.setter
     def x_scroll_pos(self, value: float):  # -1.0 will set it to max/end
-        return dpg.set_x_scroll(self.id, value)
+        return idpg.set_x_scroll(self.id, value)
 
     @property
     def x_scroll_max(self):
-        return dpg.get_x_scroll_max(self.id)
+        return idpg.get_x_scroll_max(self.id)
 
 
     def reset_pos(self):
-        dpg.reset_pos(self.id)
+        idpg.reset_pos(self.id)
 
     def children(self) -> list:
         """Returns a list containing the id of all *high-level children
         within a container.
 
-        *Slots 1 and 2 in dpg.get_item_children(self.id, slot)
+        *Slots 1 and 2 in idpg.get_item_children(self.id, slot)
         """
         return [child for slot, childs in 
-                dpg.get_item_children(self.id, -1).items()
+                idpg.get_item_children(self.id, -1).items()
                 for child in childs 
                 if any(slot == i for i in (1, 2))]
         
