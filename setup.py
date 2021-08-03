dcccd38049b63bd3ece7afb47dfafe2769aaf6ff
Anthony Doupe <atlamillias@gmail.com>
2021-08-03 11:22:03 -0700

    Added cleanup command.

--- setup.py
+++ setup.py
@@ -1,4 +1,19 @@
+import shutil
 import setuptools
 
-setuptools.setup()
+class Cleanup(setuptools.Command):
+    user_options = []
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        shutil.rmtree("./build")
+        shutil.rmtree("./dpgwidgets.egg-info")
+
+
+setuptools.setup(cmdclass = {"clean": Cleanup})
 
