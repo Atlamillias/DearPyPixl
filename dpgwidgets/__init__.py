7c92c60070d807a909823d59cbf6b66627a19818
Anthony Doupe <atlamillias@gmail.com>
2021-08-09 23:13:50 -0700

    Now calls setup_registries.

--- dpgwidgets/__init__.py
+++ dpgwidgets/__init__.py
@@ -11,9 +11,7 @@ all of their hard work in creating DearPyGui
 """
 import sys
 sys.path.extend(["./libsrc", "./tools"])
-
 import warnings
-import dearpygui.dearpygui as dearpygui
 from . import _writelib
 
 # NOTE: For releases/distributions that include this library, I'd
@@ -29,29 +27,19 @@ def update_library():
 if AUTO_UPDATE and __name__ != "__main__":
     update_library()
 
-# setting up registries
-from dpgwidgets.constants import Registry as _Registry, Key, Mouse
 
-with dearpygui.font_registry(id=_Registry.FONT.value, label="AppFontRegistry"):
-    pass
-with dearpygui.handler_registry(id=_Registry.APPHANDLER.value, label="AppHandlerRegistry"):
-    pass
-with dearpygui.texture_registry(id=_Registry.TEXTURE.value, label="AppTextureRegistry"):
-    pass
-with dearpygui.value_registry(id=_Registry.VALUE.value, label="AppValueRegistry"):
-    pass
+from dearpygui.dearpygui import setup_registries as _setup_registries
+_setup_registries()
 
 
 
-
-import dpgwidgets.libsrc as dpglib  # formerly dpgwrap
-
+import dpgwidgets.libsrc as dpglib
 from dpgwidgets.app import Viewport, _Viewport
 from dpgwidgets.theme import Font, Theme
 from dpgwidgets.constants import Key, Mouse
 
 
-__version__ = "0.2.33"
+__version__ = "0.2.39"
 
 __all__ = [
     # high-level objects
@@ -61,7 +49,7 @@ __all__ = [
     "Font",
     "Key",
     "Mouse",
-    # imports
+    # modules
     "containers",
     "drawing",
     "node",
@@ -70,18 +58,8 @@ __all__ = [
     "theme",
     "themes",
     "tools",
-    # wrapped lib
-    "dpglib",
 ]
 
 
-# Typically, UI code is in main.py and not split across other
-# modules. For larger projects though it is better for organization.
-# "Application" exists to make it easier to split a UI across
-# multiple files. Instead of creating a Viewport instance in
-# one file and having every other file import it, they can import
-# Application instead.
-#
-# When an instance of the real Viewport class is created, the
-# variable below is re-bound to the real one.
+
 Application = _Viewport()
