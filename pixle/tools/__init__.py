07983c06b18822e764ffc02b83b1c6c6870afb73
Anthony Doupe <atlamillias@gmail.com>
2021-09-02 17:17:22 -0700

    Added new helper functions.

--- pixle/tools/__init__.py
+++ pixle/tools/__init__.py
@@ -1,3 +1,5 @@
+from contextlib import contextmanager
+from dearpygui import _dearpygui, dearpygui
 from pixle.tools.dpgtools import (
     show_about,
     show_debug,
@@ -9,3 +11,34 @@ from pixle.tools.dpgtools import (
 )
 from pixle.tools.pyconsole import PyConsole
 from pixle.tools.appinfo import AppInfo
+
+
+
+@contextmanager
+def stage():
+    """Sets staging mode for all items added within the call's context.
+    An item created in staging mode is not rendered and has limited
+    functionality until its `unstage` method is called. Useful for
+    creating items whos parents will exist later.
+
+    Yields:
+        None
+    """
+    try:
+        yield _dearpygui.set_staging_mode(True)
+    finally:
+        _dearpygui.set_staging_mode(False)
+
+
+@contextmanager
+def mutex():
+    """Locks the mutex within context of this call. Mutex is unlocked
+    once out-of-scope.
+
+    Yields:
+        None
+    """
+    try:
+        yield _dearpygui.lock_mutex()
+    finally:
+        _dearpygui.unlock_mutex()
