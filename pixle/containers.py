fba87d1975502ac800b4dfb7690be1c26b507ffa
Anthony Doupe <atlamillias@gmail.com>
2021-09-13 12:28:29 -0700

    StagingContainer, Tooltip

--- pixle/containers.py
+++ pixle/containers.py
@@ -1,4 +1,4 @@
-from typing import Callable, Any
+from typing import Callable, Any, Union
 
 from dearpygui import dearpygui, _dearpygui
 
@@ -57,3 +57,79 @@ class Popup(Window):
     def __on_click_callback(self):
         self.configure(show=True)
 
+
+class StagingContainer(Container):
+    """Undocumented function
+    Args:
+            **label (str): Overrides 'name' as label.
+            **user_data (Any): User data for callbacks.
+            **use_internal_label (bool): Use generated internal label instead of user specified (appends ### uuid).
+            **id (Union[int, str]): Unique id used to programmatically refer to the item. If label is unused this will be the label.
+    Returns:
+            Union[int, str]
+    
+    """
+    _command = dearpygui.add_staging_container
+
+    def __init__(
+        self,
+        label: str = None,
+        user_data: Any = None,
+        use_internal_label: bool = True,
+        **kwargs,
+    ):
+        super().__init__(
+            label=label,
+            user_data=user_data,
+            use_internal_label=use_internal_label,
+            **kwargs,
+        )
+        self.label = label
+        self.user_data = user_data
+        self.use_internal_label = use_internal_label
+
+    def __enter__(self):
+        _dearpygui.set_staging_mode(True)
+        super().__enter__()
+
+    def __exit__(self, exec_type, exec_value, traceback):
+        super().__exit__(exec_type, exec_value, traceback)
+        _dearpygui.set_staging_mode(False)
+
+
+class Tooltip(Container):
+    """Adds an advanced tool tip for an item. This command must come immediately after the item the tip is for.
+    Args:
+            parent (Union[int, str]): 
+            **label (str): Overrides 'name' as label.
+            **user_data (Any): User data for callbacks.
+            **use_internal_label (bool): Use generated internal label instead of user specified (appends ### uuid).
+            **id (Union[int, str]): Unique id used to programmatically refer to the item.If label is unused this will be the label.
+            **show (bool): Attempt to render widget.
+    Returns:
+            Union[int, str]
+    
+    """
+    _command = dearpygui.add_tooltip
+
+    def __init__(
+        self,
+        parent: Union[int, str],
+        label: str = None,
+        user_data: Any = None,
+        use_internal_label: bool = True,
+        show: bool = True,
+        **kwargs,
+    ):
+        super().__init__(
+            parent=parent,
+            label=label,
+            user_data=user_data,
+            use_internal_label=use_internal_label,
+            show=show,
+            **kwargs,
+        )
+        self.label = label
+        self.user_data = user_data
+        self.use_internal_label = use_internal_label
+        self.show = show
