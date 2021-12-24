aa1550161b014fae9169dec322d96b97bb997270
Anthony Doupe <75037301+Atlamillias@users.noreply.github.com>
2021-12-24 01:53:29 -0700

    Wrapped DPG's initialization system.

--- dearpypixl/__init__.py
+++ dearpypixl/__init__.py
@@ -1,4 +1,50 @@
-from dearpypixl.application import Application as _Application, Viewport as _Viewport
+""""""
+_is_initialized: bool = False
 
-Application = _Application()
-Viewport = _Viewport()
+
+def initialize_dearpypixl():
+    """Perform setup for DearPyGui and DearPyPixl if it has not been
+    done already. Does nothing if already initialized.
+    """
+    if _is_initialized is False:
+        from dearpygui import dearpygui
+        # Prepare DearPyGui for use.
+        dearpygui.create_context()
+        dearpygui.create_viewport()  # created but not shown
+        dearpygui.setup_dearpygui()
+        dearpygui.configure_viewport("DPG NOT USED YET", title="Application")
+        # DearPyPixl does not expose this registry. The `Font` class
+        # manages all font-related items on its own. All this does is
+        # create a default parent for font items.
+        # TODO: Probably move this to the `Font` class. Once it's refactored...
+        dearpygui.add_font_registry(tag=dearpygui.mvReservedUUID_0)
+
+
+def cleanup_dearpypixl():
+    """Release resources and destroy contexts used by DearPyGui.
+    Does nothing if not initialized.
+    """
+    if _is_initialized is True:
+        from dearpygui import dearpygui
+        try:
+            dearpygui.destroy_context()
+        except:
+            pass
+
+
+# I try to avoid calls to dearpygui on dearpypixl module/package import, but
+# initializing before the imports just to be safe (it's annoying to debug an
+# on-import segfault).
+initialize_dearpypixl()
+
+
+from dearpypixl.application.application import Application
+from dearpypixl.application.viewport import Viewport
+from dearpypixl.components import (
+    Theme,
+    ThemeColorComponent,
+    ThemeComponent,
+    ThemeStyleComponent,
+    AppEvents,
+    ItemEvents
+)
