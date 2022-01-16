7255da3370b00fd8ea795234cbbaf2a7cf29abf6
Anthony Doupe <75037301+Atlamillias@users.noreply.github.com>
2022-01-15 22:25:41 -0700

    __init_subclass__ is used instead of __new__

--- dearpypixl/application/__init__.py
+++ dearpypixl/application/__init__.py
@@ -1,16 +1,25 @@
 from typing import Callable
-from abc import abstractmethod, ABCMeta
-from dearpypixl.components import ItemAttribute, Item
+from abc import abstractmethod, ABCMeta, ABC
+from dearpypixl.components import ItemAttribute, Item, ProtoItem
 
 
 
-class AppItemType(ABCMeta):
-    # NOTE: 'ProtoItem` must be included in a class's mro when using `AppItemType` as a metaclass. 
-    __slots__    = ()
-    __instance__ = None
-    __repr__     = Item.__repr__
-    __int__      = Item.__int__
+class AppItemMeta(type):
+    def __setattr__(cls, name, value):
+        # Workaround to emulate a set-able classmethod property without
+        # overwriting the descriptor.
+        if name in cls._item_config_attrs:
+            return setattr(cls.__instance__, name, value)
+        super().__setattr__(name, value)
+    
+    def __repr__(cls) -> str:
+        return f"{cls.__qualname__}(tag={cls.tag!r})"
 
+    def __int__(cls)  -> int:
+        return cls.tag
+
+
+class AppItem(ProtoItem, ABC, metaclass=type("AppItemType", (AppItemMeta, ABCMeta), {})):
     @abstractmethod
     def tag()           -> int | str: ...
     @abstractmethod
@@ -20,24 +29,17 @@ class AppItemType(ABCMeta):
     @abstractmethod
     def state()         -> Callable : ...
 
-    def __new__(mcls, name, bases, namespace, **kwargs):
-        cls = super().__new__(mcls, name, bases, namespace, **kwargs)
-        if not cls.__instance__:
-            # Fallback instance for setting configuration properties when set on the
-            # class instead of an instance.
+    __slots__    = ()
+    __instance__ = None
+    
+    def __init_subclass__(cls):
+        super().__init_subclass__()
+        if cls.mro()[1] == AppItem:
             cls.__instance__ = cls()
-        return cls
-
-    def __setattr__(cls, name, value):
-        # Workaround to emulate a set-able classmethod property without
-        # overwriting the descriptor.
-        if name in cls._item_config_attrs:
-            return setattr(cls.__instance__, name, value)
-        type.__setattr__(cls, name, value)
 
 
 class AppAttribute(ItemAttribute):
-    def __get__(self, instance: object, _type: AppItemType):
+    def __get__(self, instance: object, _type: AppItem):
         # Effectively a classmethod property.
         if not _type.__instance__:
             return self
