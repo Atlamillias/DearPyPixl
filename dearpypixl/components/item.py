5f42445b98f6a0f934ab18619a36c73bb7786788
Anthony Doupe <75037301+Atlamillias@users.noreply.github.com>
2022-02-11 20:24:04 -0700

    `descendants_only` added to `item_tree` method.

--- dearpypixl/components/item.py
+++ dearpypixl/components/item.py
@@ -28,6 +28,9 @@ from dearpypixl.constants import ItemIndex
 from dearpypixl.components.configuration import (
     item_attribute,
     ItemAttribute,
+    CONFIGURATION,
+    INFORMATION,
+    STATE
 )
 
 __all__ = [
@@ -96,7 +99,7 @@ class ProtoItem(metaclass=ABCMeta):
     intention of passing them onto descendants as internally managed attributes.
     """
     # NOTE: ALL Item-related class objects in DearPyPixl should be inheriting from
-    #       this in some fashion. This includes the `Item` base type, and ALL
+    #       this in some fashion. This includes the `Item` base type and ALL
     #       (App)Item mixin classes. Default implementations for exposing the registered
     #       managed attributes is defined on `Item`.
     __slots__ = ()
@@ -122,7 +125,7 @@ class ProtoItem(metaclass=ABCMeta):
         # Import categorized item attributes registered from `ItemAttribute`,
         # `item_attribute`.
         cls._item_config_attrs = [*ItemAttribute.next_class_item_attrs["configuration"]]
-        cls._item_inform_attrs = [*ItemAttribute.next_class_item_attrs["information"]]
+        cls._item_inform_attrs = [*ItemAttribute.next_class_item_attrs[INFORMATION]]
         cls._item_states_attrs = [*ItemAttribute.next_class_item_attrs["state"]]
         cls._item_cached_attrs = [*ItemAttribute.next_class_item_attrs["cached"]]
         # Reset `next_class_item_attrs.values()` for the next class.
@@ -167,141 +170,27 @@ class ProtoItem(metaclass=ABCMeta):
 
 
 class Item(ProtoItem, metaclass=ABCMeta):
-    """
-    Abstract Methods
-    ----------------
-    * _command (Callable): Class attribute - Internal API command that can
-    be called to create the item.
+    """Base class for all wrapped DearPyGui item types.
 
 
-    Properties
-    ----------
-
+    Attributes & Properties
+    -----------------------
+        * unique_parents (class property, read-only)
+        * unique_children (class property, read-only)
+        * is_root_item (class property, read-only)
+        * is_container (class property, read-only)
+        * is_ok (read-only)
+        * is_enabled (read-only)
+        * tag (read-only)
+        * parent (read-only)
 
 
     Methods
     -------
+        * raw_init (classmethod)
+        * as_template (classmethod)
 
     """
-    __slots__ = ("_tag",)
-
-    _is_container    : bool  = False
-    _is_root_item    : bool  = False
-    _is_value_able   : bool  = False
-    _unique_parents  : tuple[str, ...] = ()
-    _unique_children : tuple[str, ...] = ()
-    _unique_commands : tuple[str, ...] = ()
-    _unique_constants: tuple[str, ...] = ()
-
-
-
-    @abstractmethod
-    def _command() -> Callable[..., Any]: ...
-
-    def __init__(self, **kwargs):
-        self._tag = kwargs.pop("tag", generate_uuid())
-
-        _cached_attrs = self._item_cached_attrs
-
-        # `user_data` needs to be excluded from type casting as it is allowed
-        # to be anything.
-        user_data = kwargs.pop("user_data", None)
-        for arg, val in kwargs.items():
-            # Recasting int-like values.
-            if isinstance(val, (Item, IntEnum)):
-                kwargs[arg] = int(val)
-            # Storing attributes that cannot be fetched from DPG on instance. 
-            if arg in _cached_attrs:
-                setattr(self, f"_{arg}", val)
-
-        type(self)._command(tag=self._tag, user_data=user_data, **kwargs)
-        self._AppItemsRegistry[self._tag] = self  # Registering Item obj
-
-    def __repr__(self):
-        return f"{type(self).__qualname__}(tag={self._tag!r})"
- 
-    def __int__(self):
-        return self._tag
-
-    def __iter__(self):
-        yield from self.children()
-
-    def __hash__(self):
-        return hash((type(self), self._tag))
-
-    def __eq__(self, other):
-        if not isinstance(other, ProtoItem):
-            return False
-        return hash(self) == hash(other)
-
-
-    ## list-like methods
-    @functools.singledispatchmethod
-    def __getitem__(self, value):
-        raise NotImplemented("This slice or index operation is not supported.")
-
-    @__getitem__.register
-    def __getslice(self, value: slice) -> list[ItemT]:
-        # `value` is a slice: `[0:4:0]`, `[:]`
-        return self.children(slot=1)[value.start: value.stop: value.step]
-
-    @__getitem__.register
-    def __getindex(self, value: int) -> ItemT:
-        # `value` is an index: `[0]`
-        # Not calling the `children` method as it is unnecessarily slow only to fetch 1 item.
-        child_idx = get_item_info(self._tag)["children"][1][value]
-        return self._AppItemsRegistry[child_idx]
-
-    def index(self, __value: ItemT, __start: int = 0, __stop: int = None) -> int:
-        """Return first index of `value`. Raises `ValueError` if the value is not present.
-        """
-        children = self.children(slot=1)
-        return children.index(__value, __start, __stop or len(children))
-
-    def copy(self, recursive: bool = False, **config) -> Self:
-        """Create and return a copy of this item. Children of the copied item
-        will not be duplicated unless `recursive` is True.
-
-        Args:
-            * recursive (bool, optional): If True, all item children will also be
-            duplicated. Default is False.
-            * config (keyword-only, optional): Configuration that will override
-            the copied configuration for the returned Item instance.
-        """
-        cls = type(self)
-        configuration = self.configuration() | config
-
-        if configuration.get("pos", None) == [0, 0]:
-            configuration["pos"] = []
-
-        if not cls._is_root_item and "parent" not in config:
-            configuration["parent"] = self.parent
-
-        # `value` isn't typically a valid argument. Usually it's `default_value`,
-        # but it's not consistent. To be safe, the value is set after item
-        # creation.
-        value = configuration.pop("value", None)
-        self_copy = cls(**configuration)
-        # Applying value (if the item isn't value-able then it simply won't "stick")
-        dearpygui.set_value(self_copy._tag, value)
-
-        if recursive:
-            for child in self.children():
-                child.copy(recursive=recursive, parent=self_copy)
-
-        return self_copy
-
-
-    @classmethod
-    @property
-    @item_attribute(category="information")
-    def _item_index(cls) -> ItemIndex:
-        """The enum member that represents the item's type.
-        """
-        try:
-            return ItemIndex[cls.__qualname__]
-        except (AttributeError, KeyError):
-            return None
 
     @classmethod
     def raw_init(cls, **config) -> Union[int, str]:
@@ -336,7 +225,7 @@ class Item(ProtoItem, metaclass=ABCMeta):
 
     @classmethod
     @property
-    @item_attribute(category="information")
+    @item_attribute(category=INFORMATION)
     def unique_parents(cls) -> tuple[str, ...]:
         """Return the names of all item types that can parent this item IF the item requires
         specific parent items. If the returned tuple is empty, then the item can be parented by
@@ -346,7 +235,7 @@ class Item(ProtoItem, metaclass=ABCMeta):
 
     @classmethod
     @property
-    @item_attribute(category="information")
+    @item_attribute(category=INFORMATION)
     def unique_children(cls) -> tuple[str, ...]:
         """Return the names of all item types that this item can parent. If the returned tuple
         is empty, then this type of item can parent most non-root items.
@@ -355,30 +244,38 @@ class Item(ProtoItem, metaclass=ABCMeta):
 
     @classmethod
     @property
-    @item_attribute(category="information")
+    @item_attribute(category=INFORMATION)
     def is_container(cls) -> bool:
-        """Return True if this item is capable of parenting other items.
+        """Return True if items of this type are capable of parenting other items.
         """
         return cls._is_container
 
     @classmethod
     @property
-    @item_attribute(category="information")
+    @item_attribute(category=INFORMATION)
     def is_root_item(cls) -> bool:
         """Return True if this item does not require a parent item to exist
         (and cannot be parented by other items).
         """
         return cls._is_root_item
+    
+    @classmethod
+    @property
+    @item_attribute(category=INFORMATION)
+    def is_value_able(cls) -> bool:
+        """Return True if items of this type can hold a value.
+        """
+        return cls._is_value_able
 
     @property
-    @item_attribute(category="information")
+    @item_attribute(category=INFORMATION)
     def tag(self) -> int:
         """This item's unique identifier.
         """
         return self._tag
 
     @property
-    @item_attribute(category="information")
+    @item_attribute(category=INFORMATION)
     def parent(self) -> 'Item':
         """Return the direct progenitor of this item.
         """
@@ -419,7 +316,7 @@ class Item(ProtoItem, metaclass=ABCMeta):
     def information(self) -> dict[str, Any]:
         """Return various read-only information about the item.
         """
-        # NOTE: The only two keys in the return that can be used for item creation
+        # The only two keys in the return that can be used for item creation
         # are `tag` and `parent`. However, they are read-only so they're included here.
         return {attr: getattr(self, attr) for attr in self._item_inform_attrs}
 
@@ -427,10 +324,7 @@ class Item(ProtoItem, metaclass=ABCMeta):
         """Return the current state(s) of the item.
         """
         # Similar to the speed-up implementation for the `configuration` method
-        # but a little more hacky (and for less of a performance boost).
-        # From cProfile, calling `self.state()` 50,000 times:
-        #   * `getattr` everything (descriptors): 1.27 seconds
-        #   * `getattr` only if necessary: 0.810 seconds
+        # but a little more hacky (and for less 'profit').
         states = {f"is_{state}":v for state, v in get_item_state(self._tag).items()}
         item_states_attrs = self._item_states_attrs
         return {attr:(states[attr] if attr in states else
@@ -452,10 +346,48 @@ class Item(ProtoItem, metaclass=ABCMeta):
         appitems    = self._AppItemsRegistry
         return tuple([appitems[child_id] for child_id in child_ids])
 
-    def focus(self) -> None:
-        """Brings a visual item into focus. Does nothing to un-viewable items.
+    def index(self, __value: ItemT, __start: int = 0, __stop: int = None) -> int:  # mimic list method
+        """Return first index of `value`. Raises `ValueError` if the value is not present.
         """
-        focus_item(self._tag)
+        children = self.children(slot=1)
+        return children.index(__value, __start, __stop or len(children))
+
+    def item_tree(self, descendants_only: bool = False) -> dict['Item', list[list['Item', list]]]:
+        """Return the entire parential tree that includes the item starting from the
+        root parent. Non-table items are represented as 2-item lists `[item, [childs]]`
+        where each child of that item is also represented as a 2-item list `[item, [childs]]`.
+        Table items (only `mvAppItemType::Table`) are represented as
+        `[table_item, [rows], [columns]]`.
+
+        `FileExtension`, `FontRangeHint`, `NodeLink`, `Annotation`, `DragLine`,
+        `DragPoint`, `DragPayload`, and `Legend` items are excluded from the tree.
+
+        Args:
+            * item (int | str): The tag of an item.
+            * descendants_only (bool, optional): If True, the tree will start from <item>
+            and not its root parent. Default is False.
+        """
+        root_item = self if descendants_only else None
+        current_item = self
+        while root_item is None:
+            if current_item.is_root_item:
+                root_item = current_item
+            current_item = current_item.parent
+        
+        tree = root_item._item_tree()
+        return {tree[0]: tree[1]}
+        
+    def _item_tree(self) -> list['Item', list['Item', list]]:
+        tree        = [self, []]
+        child_slots = [*get_item_info(self._tag)["children"].values()]
+        # slots 1 (most items) and 2 (draw items)
+        appitems = self._AppItemsRegistry
+        children = [*child_slots[1], *child_slots[2]]
+        for child in children:
+            child = appitems[child]
+            child_tree = child._item_tree()
+            tree[1].append(child_tree)
+        return tree
 
     def move(self, parent: Union[ItemT, int] = 0, before: Union[ItemT, int] = 0) -> None:
         """If the item is a child, it will be appended to <parent>'s list
@@ -483,18 +415,38 @@ class Item(ProtoItem, metaclass=ABCMeta):
         """
         move_item_down(self._tag)
 
-    def unstage(self) -> None:
-        """Sets this item to be rendered as normal if it has been staged.
+    def copy(self, recursive: bool = False, **config) -> Self:  # kinda mimic list method
+        """Create and return a copy of this item. Children of the copied item
+        will not be duplicated unless `recursive` is True.
 
-        NOTE: An item is **staged** when it is the child of a `Stage` or
-        Stage-like item. Staged items are created and can be configured,
-        but are not set for rendering until they have been **unstaged** or
-        **moved**. 
+        Args:
+            * recursive (bool, optional): If True, all item children will also be
+            duplicated. Default is False.
+            * config (keyword-only, optional): Configuration that will override
+            the copied configuration for the returned Item instance.
         """
-        try:
-            unstage(self._tag)
-        except SystemError:
-            pass
+        cls = type(self)
+        configuration = self.configuration() | config
+
+        if configuration.get("pos", None) == [0, 0]:
+            configuration["pos"] = []
+
+        if not cls._is_root_item and "parent" not in config:
+            configuration["parent"] = self.parent
+
+        # `value` isn't typically a valid argument. Usually it's `default_value`,
+        # but it's not consistent. To be safe, the value is set after item
+        # creation.
+        value = configuration.pop("value", None)
+        self_copy = cls(**configuration)
+        # Applying value (if the item isn't value-able then it simply won't "stick")
+        dearpygui.set_value(self_copy._tag, value)
+
+        if recursive:
+            for child in self.children():
+                child.copy(recursive=recursive, parent=self_copy)
+
+        return self_copy
 
     def delete(self, children_only: bool = False, slot: int = -1) -> None:
         """Deletes the item and all children.
@@ -522,27 +474,103 @@ class Item(ProtoItem, metaclass=ABCMeta):
         if not children_only:
             del self
 
-    def item_tree(self) -> dict['Item', list[list['Item', list]]]:
-        """Return the entire parential tree for this item.
+    def focus(self) -> None:
+        """Brings a visual item into focus. Does nothing to un-viewable items.
         """
-        # Find the root parent first.
-        root_item    = None
-        current_item = self
-        while root_item is None:
-            if current_item.is_root_item:
-                root_item = current_item
-            current_item = current_item.parent
-        
-        tree = root_item.__item_tree()
-        return {tree[0]: tree[1]}
+        focus_item(self._tag)
 
-    def __item_tree(self) -> list['Item', list['Item', list]]:
-        tree = [self, []]
-        for child in self.children():
-            child_tree = child.__item_tree()
-            tree[1].append(child_tree)
-        return tree
+    def unstage(self) -> None:
+        """Sets this item to be rendered as normal if it has been staged.
 
+        NOTE: An item is **staged** when it is the child of a `Stage` or
+        Stage-like item. Staged items are created and can be configured,
+        but are not set for rendering until they have been **unstaged** or
+        **moved**. 
+        """
+        try:
+            unstage(self._tag)
+        except SystemError:
+            pass
+
+    ######################
+    ###### END API #######
+    ######################
+
+    __slots__ = ("_tag",)
+
+    _is_container    : bool  = False
+    _is_root_item    : bool  = False
+    _is_value_able   : bool  = False
+    _unique_parents  : tuple[str, ...] = ()
+    _unique_children : tuple[str, ...] = ()
+    _unique_commands : tuple[str, ...] = ()
+    _unique_constants: tuple[str, ...] = ()
+
+    @abstractmethod
+    def _command() -> Callable[..., Any]: ...
+
+
+    def __init__(self, **kwargs):
+        self._tag = kwargs.pop("tag", generate_uuid())
+
+        cached_attrs = self._item_cached_attrs
+        # `user_data` needs to be excluded from type casting as it is allowed
+        # to be anything.
+        user_data = kwargs.pop("user_data", None)
+        for arg, val in kwargs.items():
+            # Recasting int-like values.
+            if isinstance(val, (Item, IntEnum)):
+                kwargs[arg] = int(val)
+            # Storing attributes that cannot be fetched from DPG on instance. 
+            if arg in cached_attrs:
+                setattr(self, f"_{arg}", val)
+
+        type(self)._command(tag=self._tag, user_data=user_data, **kwargs)
+        self._AppItemsRegistry[self._tag] = self  # Registering Item obj
+
+    def __repr__(self):
+        return f"{type(self).__qualname__}(tag={self._tag!r})"
+ 
+    def __int__(self):
+        return self._tag
+
+    def __iter__(self):
+        yield from self.children()
+
+    def __hash__(self):
+        return hash((type(self), self._tag))
+
+    def __eq__(self, other):
+        if not isinstance(other, ProtoItem):
+            return False
+        return hash(self) == hash(other)
+
+    # list-like stuff
+    @functools.singledispatchmethod
+    def __getitem__(self, value):
+        raise NotImplemented("This slice or index operation is not supported.")
+
+    @__getitem__.register
+    def __getslice(self, value: slice) -> list[ItemT]:
+        # `value` is a slice: `[0:4:0]`, `[:]`
+        return self.children(slot=1)[value.start: value.stop: value.step]
+
+    @__getitem__.register
+    def __getindex(self, value: int) -> ItemT:
+        # `value` is an index: `[0]`
+        # Not calling the `children` method as it is unnecessarily slow only to fetch 1 item.
+        child_idx = get_item_info(self._tag)["children"][1][value]
+        return self._AppItemsRegistry[child_idx]
+
+    @classmethod
+    @property
+    def _item_index(cls) -> ItemIndex:
+        """The enum member that represents the item's type.
+        """
+        try:
+            return ItemIndex[cls.__qualname__]
+        except (AttributeError, KeyError):
+            return None
 
     #### Work-In-Progress ####
     def _export_configuration(self) -> dict[str, Any]:
