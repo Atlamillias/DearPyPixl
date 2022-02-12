ff3c17ee6311508162c514d8f91d6d49d40eb207
Anthony Doupe <75037301+Atlamillias@users.noreply.github.com>
2022-02-11 20:25:42 -0700

    Removed unnecessary repeated code.

--- dearpypixl/colors.py
+++ dearpypixl/colors.py
@@ -4,33 +4,12 @@ from dearpypixl.appitems.colors import *
 from typing import Union, Any
 from dearpygui import dearpygui
 
+
 __all__ = [*dearpypixl.appitems.colors.__all__]
 
 
 class ColorMap(ColorMap):
     # TODO: Maybe make list-like for access to `colors`.
-    def __init__(
-        self                                                             ,
-        colors            : list[Union[list[int], tuple[int, ...]]]      ,
-        qualitative       : bool                                         ,
-        label             : str                                    = None,
-        user_data         : Any                                    = None,
-        use_internal_label: bool                                   = True,
-        show              : bool                                   = True,
-        parent            : Union[int, str]                        = 14  ,
-        **kwargs                                                         ,
-    ) -> None:
-        super().__init__(
-            colors=colors,
-            qualitative=qualitative,
-            label=label,
-            user_data=user_data,
-            use_internal_label=use_internal_label,
-            show=show,
-            parent=parent,
-            **kwargs,
-        )
-
     def get_color_at_value(self, value: float) -> Union[list[float], tuple[float, ...]]:
         """Return the normalized values of a color from the ColorMap at <value>.
         Up to four values can be returned in the sequence: red, green, blue, alpha.
