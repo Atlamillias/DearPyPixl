24be4ad8310ff22188d65e1e35827c6c0d396a87
Anthony Doupe <atlamillias@outlook.com>
2022-05-05 16:55:56 -0700

    Added properties/methods to `PlotAxis`.

--- dearpypixl/plotting.py
+++ dearpypixl/plotting.py
@@ -13,14 +13,39 @@ __all__ = [
 
 
 class PlotAxis(PlotAxis):
+    @property
+    def axis_limits(self) -> tuple[float, float]:
+        return dpg.get_axis_limits(self._tag)
+    @axis_limits.setter
+    def axis_limits(self, value: tuple[float, float]) -> None:
+        self.set_limits(*value)
+
+    def get_limits(self) -> tuple[float, float]:
+        """Return the current axis limits (ymin, ymax).
+        """
+        return dpg.get_axis_limits(self._tag)
+
     def set_limits(self, ymin: float | int = None, ymax: float | int = None, **kwargs) -> None:
-        """Sets zoom and pan limits for the axis.
+        """Sets zoom and pan limits for the axis (ymin, ymax).
 
         Args:
             * ymin (float | int, optional):
             * ymax (float | int, optional):
         """
-        return dpg.set_axis_limits(self.tag, ymin, ymax, **kwargs)
+        try:
+            dpg.set_axis_limits(self.tag, ymin, ymax, **kwargs)
+        except SystemError:
+            self._err_if_existential_crisis()
+            raise
+
+    def set_limits_fit_data(self, **kwargs) -> None:
+        """Set the axis limits (ymin, ymax) to fit the largest set of data parented by the axis.
+        """
+        try:
+            dpg.fit_axis_data()
+        except SystemError:
+            self._err_if_existential_crisis()
+            raise
 
     def set_ticks(self, label_pairs: tuple[tuple[str, float | int], ...], **kwargs) -> None:
         """Replace the default axis ticks with <label_pairs>.
@@ -30,17 +55,17 @@ class PlotAxis(PlotAxis):
             The first item of each inner tuple is the tick label, while the second is the value
             at which to set the tick.
         """
-        return dpg.set_axis_ticks(self.tag, label_pairs, **kwargs)
+        return dpg.set_axis_ticks(self.tag, label_pairs)
 
     def reset_limits(self, **kwargs) -> None:
         """Remove any manually-set axis limits, and apply the default limits.
         """
-        return dpg.set_axis_limits_auto(self.tag, **kwargs)
+        return dpg.set_axis_limits_auto(self.tag)
 
     def reset_ticks(self, **kwargs) -> None:
         """Remove any manually-set axis ticks, and apply the default ticks.
         """
-        return dpg.reset_axis_ticks(self.tag, **kwargs)
+        return dpg.reset_axis_ticks(self.tag)
 
 
 class PlotAxisX(PlotAxis):
