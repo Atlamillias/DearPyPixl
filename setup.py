b28ede22f986ce6ae436230c5852473e42546bdf
Anthony Doupe <atlamillias@outlook.com>
2023-01-25 18:19:37 -0700

    misc (setup): rm v0.3 setup

--- setup.py
+++ setup.py
@@ -3,26 +3,5 @@ import setuptools
 import subprocess
 
 
-class Cleanup(setuptools.Command):
-    user_options = []
+# TODO: New build script
 
-    def initialize_options(self):
-        pass
-
-    def finalize_options(self):
-        pass
-
-    def run(self):
-        print("Building...")
-        subprocess.run(["python", "setup.py", "sdist", "bdist_wheel"])
-        print("\nCleaning up...", end="")
-        dirs = ("./build", "./dearpypixl.egg-info")
-        for dir in dirs:
-            try:
-                shutil.rmtree(dir)
-            except OSError:
-                pass
-        print("done.")
-
-
-setuptools.setup(cmdclass = {"process": Cleanup})
