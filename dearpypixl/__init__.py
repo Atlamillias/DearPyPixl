df8c7f080dbef72744f2c637f5a695470f74ada8
Anthony Doupe <atlamillias@outlook.com>
2023-03-23 19:39:57 -0700

    misc: new module `px_patcher`

--- dearpypixl/__init__.py
+++ dearpypixl/__init__.py
@@ -1,27 +1,11 @@
 """A lightweight framework and toolkit for DearPyGui."""
 
-# public imports, in order of lib dependency; none/little (top) to alot/all (bottom)
+from . import px_patcher as _px_patcher
+
+_px_patcher.apply_patch()
+
+
 try:
     from dearpypixl.appitems import *
 except ImportError:
     pass
-
-
-
-def _optimize(mod = None) -> None:
-    """Replaces a few Python objects used in performance-critical areas with
-    an equivelent written in rust."""
-    import sys
-    from . import grid
-
-    # TODO: crossplatform optimization
-    if not mod and sys.platform() != "win32":
-        return
-    try:
-        from . import _dearpypixl
-    except:
-        return
-
-    grid._draw_cells = _dearpypixl.grid.draw_cells
-
-
