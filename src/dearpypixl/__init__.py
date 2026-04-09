5ed4673f81ac07c5fc24ffb33c4236ed6c7b0160
Anthony Doupe <atlamillias@outlook.com>
2026-04-09 09:48:36 -0700

    fix: issue warning BEFORE importing symbols

--- src/dearpypixl/__init__.py
+++ src/dearpypixl/__init__.py
@@ -1,6 +1,12 @@
 import sys as _sys
 from dearpypixl.core import management as _management
 
+if "dearpygui" in _sys.modules:
+    _management.warn(
+        "`dearpypixl` should be imported before `dearpygui` — "
+        "references to objects in `dearpygui` may be invalidated",
+        stacklevel=2
+    )
 
 from dearpypixl.core.protocols import mvBuffer as mvBuffer
 from dearpypixl.core.protocols import mvVec4 as mvVec4
@@ -11,14 +17,6 @@ from dearpypixl.core.appitem import ChildItem as ChildItem
 from dearpypixl.core.appitem import CompositeItem as CompositeItem
 
 if not _management._DPX_NO_INIT:
-
-    if "dearpygui" in _sys.modules:
-        _management.warn(
-            "`dearpypixl` should be imported before `dearpygui` — "
-            "references to objects in `dearpygui` may be invalidated",
-            stacklevel=2
-        )
-
     from dearpypixl.lib.application import *
     from dearpypixl.lib.viewport import *
     from dearpypixl.lib.items import *
